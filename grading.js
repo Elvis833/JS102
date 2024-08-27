1672d8890459415776460b2cde929e6da92fd937
Elvis Iradukunda <student@Students-MacBook-Pro.local>
2024-08-27 14:20:18 +1200

    JS102 Exercise

---
+++ grading.js
@@ -0,0 +1,21 @@
+console.log("grading system")
+let score;
+score = 97
+if (score >= 90) {
+    console.log("your score is A congrats")
+}
+else if (score >= 80) {
+    console.log("your score is B")
+}
+else if (score >= 70) {
+    console.log("your score is C")
+}
+else if (score >= 60) {
+    console.log("your score is D")
+}
+else if (score >= 50) {
+    console.log("your score is F better luck next time")
+}
+else {
+    console.log("your score is F For FAIL")
+}
---
+++ password.js
@@ -0,0 +1,9 @@
+console.log("Password checkers")
+let password;
+password= "Passcode123";
+if (password == "Passcode123") {
+    console.log("password is granted")
+}
+else {
+    console.log ("password dinied")
+}
